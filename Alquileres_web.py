b5f35b3231aadaf32d082c147ab68e8a3e76da4b
grupocomputacion <rarapisardi@gmail.com>
2026-03-24 14:08:00 -0300

    Add files via upload

--- Alquileres_web.py
+++ Alquileres_web.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_title="NL Propiedades - Gestión", 
     page_icon="🏠", 
     layout="wide", 
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 st.image("alquileres.jpg", width=200)    
 )
 
